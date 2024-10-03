38361b8b0b86fe2e005bbd8ef1d2c6ac6defded2
My Name <kipkip9xx@gmail.com>
2024-10-03 12:10:14 +0700

    create github pipeline

--- setup/init.sh
+++ setup/init.sh
@@ -4,14 +4,22 @@ set -e -o pipefail
 echo "Fetching IAM github-action-user ARN"
 userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
 
+
+#NOTE: I'm using MacOS, which is not be able to download the iam-authentication-tool as below, so I updated the download step to use HomeBrew locally
+# ====================================================================================================
+
 # Download tool for manipulating aws-auth
-echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
-chmod +x aws-iam-authenticator
+# echo "Downloading tool..."
+# curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
+# chmod +x aws-iam-authenticator
 
 echo "Updating permissions"
-./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+# ./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+
 
 echo "Cleaning up"
-rm aws-iam-authenticator
+# rm aws-iam-authenticator
 echo "Done!"
+
+# ====================================================================================================
